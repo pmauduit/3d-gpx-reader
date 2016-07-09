db6adbe4fc039e3f6ae9b52cf12535d3a1cc0210
Pierre Mauduit <pierre.mauduit@gmail.com>
2016-07-09 12:13:57 +0200

    Putting things together
    There is now only one C++ program, which parses the GPX, and produces
    the OSG scene.

--- gpx-parser.cpp
+++ gpx-parser.cpp
@@ -2,9 +2,116 @@
 
 namespace tdgr {
 
-  GpxParser::GpxParser() {};
-  GpxParser::~GpxParser() {};
+  const double GpxParser::xyfactor = 10.0E6f;
+  const double GpxParser::zfactor = 210.0f;
+  const std::string GpxParser::GPX_ELE_FIELD = "ele";
+  /**
+   * @returns the parsed vertices.
+   */
+  osg::Vec3Array * const & GpxParser::getVertices() const {
+    return vertices;
+  }
+
+  /**
+   * Returns the elevation as a double, or NAN if unavailable.
+   *
+   */
+  double GpxParser::getElevation(OGRFeature * feat) const {
+    int eleIdx = feat->GetFieldIndex(GpxParser::GPX_ELE_FIELD.c_str());
+    if (eleIdx != -1) {
+      OGRFieldDefn * field = feat->GetFieldDefnRef(eleIdx);
+      OGRFieldType t = field->GetType();
+      if (t == OFTReal) {
+       OGRField * fvalue = feat->GetRawFieldRef(eleIdx);
+       return fvalue->Real;
+      }
+    }
+    return NAN;
+  }
 
 
+  osg::Vec3 * GpxParser::getVec3FromFeature(OGRFeature * feat) const {
+    OGRGeometry * geom = feat->GetGeometryRef();
+    double x = 0, y = 0, z;
+    z = this->getElevation(feat);
+    if (geom->getGeometryType() == wkbPoint) {
+      OGRPoint * pt = (OGRPoint *) geom;
+      x = pt->getX();
+      y = pt->getY();
+    } else {
+      OGRPoint *centroid = new OGRPoint;
+      geom->Centroid(centroid);
+      x = centroid->getX();
+      y = centroid->getY();
+      delete centroid;
+    }
+    return new osg::Vec3(x, y, z);
+  }
+
+  osg::Vec3 * GpxParser::getRelativeVec3(OGRFeature * feat, const osg::Vec3 * refvec) const {
+    osg::Vec3 * pt = this->getVec3FromFeature(feat);
+    for (unsigned int i = 0 ; i < 3 ; ++i) {
+      (*pt)[i] -= (*refvec)[i];
+      (*pt)[i] *= i == 2 ? zfactor : xyfactor;
+    }
+    return pt;
+  }
+  /**
+   * @path a string which represents the path to the GPX file
+   */
+  GpxParser::GpxParser(const char * path) {
+
+    GDALDataset * dataset = (GDALDataset *) GDALOpenEx(path, GDAL_OF_VECTOR | GDAL_OF_READONLY,
+                                                       NULL, NULL, NULL);
+    if(dataset == NULL)
+    {
+      fprintf(stderr, "Error opening GPX file %s\n", path);
+      return;
+    }
+
+    vertices = new osg::Vec3Array;
+    for (int i = 0 ; i < dataset->GetLayerCount() ; ++i) {
+      OGRLayer * layer = dataset->GetLayer(i);
+      OGRFeature * feat =  layer->GetNextFeature();
+
+      osg::Vec3 * refpt = NULL;
+      osg::Vec3 * currentpt = NULL;
+      unsigned long numfeat = 0;
+
+      while (feat != NULL) {
+        if (numfeat == 0) {
+          refpt = getVec3FromFeature(feat);
+        }
+        else {
+          // skip 100 features
+          //if (numfeat % 100 != 0) {
+          //  numfeat++;
+          //  OGRFeature::DestroyFeature(feat);
+          //  feat = layer->GetNextFeature();
+          //  continue;
+          //}
+          currentpt = getRelativeVec3(feat, refpt);
+          //dump_point3d_as_osg(currentpt, numfeat);
+          vertices->push_back(*currentpt);
+          if (currentpt != NULL) {
+            delete currentpt;
+            currentpt = NULL;
+          }
+        }
+        // Next feature
+        numfeat++;
+        OGRFeature::DestroyFeature(feat);
+        feat = layer->GetNextFeature();
+      }
+      if (refpt != NULL) {
+        delete refpt;
+        refpt = NULL;
+      }
+    }
+    delete dataset;
+  };
+
+  GpxParser::~GpxParser() {
+  };
 }
 
--- gpx-parser.h
+++ gpx-parser.h
@@ -2,15 +2,41 @@
 #define __GPX_PARSER_H
 
 #include <string>
+
+#include <gdal/gdal_priv.h>
+#include <gdal/cpl_conv.h>
+#include <gdal/ogrsf_frmts.h>
+#include <gdal/ogr_core.h>
+#include <gdal/ogr_feature.h>
+#include <gdal/ogr_geometry.h>
+
+#include <osg/Geometry>
+#include <osg/Geode>
+#include <osg/LineWidth>
+#include <osgUtil/Tessellator>
+#include <osgViewer/Viewer>
+#include <osg/ShapeDrawable>
+#include <osg/Texture2D>
+#include <osgDB/ReadFile>
+#include <osgGA/TerrainManipulator>
+
 // tdgr stands for ThreeDimensionGpxReader
 namespace tdgr {
 
   class GpxParser {
     private:
-     std::string gpxPath;
+     osg::Vec3Array * vertices;
+     osg::Vec3 * getVec3FromFeature(OGRFeature * feat)  const;
+     osg::Vec3 * getRelativeVec3(OGRFeature * feat, const osg::Vec3 * refvec) const;
+
+     double getElevation(OGRFeature * feat) const;
 
     public:
-     GpxParser();
+     static const std::string GPX_ELE_FIELD;
+     static const double xyfactor, zfactor;
+
+     GpxParser(const char * path);
+     osg::Vec3Array * const & getVertices() const;
      ~GpxParser();
   };
 }
